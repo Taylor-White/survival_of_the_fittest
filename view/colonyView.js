36d3a89f6be2c3227d8c9733c67827b179723bf9
twhite745 <twhite745@gmail.com>
2015-02-18 18:07:44 -0800

    colonyView.js

--- view/colonyView.js
+++ view/colonyView.js
@@ -1,11 +1,12 @@
 /*
-View: canvas?
-File owner:
+View: Colony View
+File owner: Taylor White
 */
 
 
-/* function to update(2d array) of the organism */
-function update(array)
+/* function to highlight the selected organism to be displayed on canvas */
+function select(i)
 {
-
+	$( "btn-group").css('border', '3px solid red');
+	$( "btn-group:nth-child("i")").css('border', '3px solid red');
 }
