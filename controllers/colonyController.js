5d269fe3769295e922ca387fe6227628e3627308
Jared Goddard <jgoddard@128-223-223-124-wireless.uoregon.edu>
2015-02-18 16:54:48 -0800

    first draft of organismController

--- controllers/colonyController.js
+++ controllers/colonyController.js
@@ -1,36 +1,36 @@
 /*
-Organism: Organism
-View: OrganismView 
+Colony: Colony
+View: ColonyView 
 File owner:
 */
 
 
-/* function for when user chooses random */
-function userRandState()
+/* function for when user chooses an organism*/
+function userSelectOrg()
 {
 
 }
 
-/* function for when the user clicks on the canvas, records the corrdinates */
-function userClick(int x, int y)
+/* function for when the user chooses to step the simulation */
+function userStepAll()
 {
 
 }
 
-/* function for when the user wants to clear the canvas */
-function userClear()
+/* function for when the user wants to run the whole simulation out */
+function userRunAll()
 {
 
 }
 
-/* function for when the user wants to change the organism they are using */
-function changeSelectOrg(Organism)
+/* function for when the user wants to stop the simulation from stepping */
+function UserStepEndAll()
 {
 
 }
 
-/* function for when the organism changes from alive or dead */
-function stateChanged()
+/* function for when the user wants to pause the simulation */
+function userPause()
 {
 
 }
--- controllers/organismController.js
+++ controllers/organismController.js
@@ -1,36 +1,37 @@
 /*
-Colony: Colony
-View: ColonyView 
-File owner:
+Organism: Organism
+View: OrganismView 
+File owner: Jared
 */
 
 
-/* function for when user chooses an organism*/
-function userSelectOrg()
+/* function for when user chooses random */
+function userRandState()
 {
-
+	Organism.randState();
 }
 
-/* function for when the user chooses to step the simulation */
-function userStepAll()
+/* function for when the user clicks on the canvas, records the coordinates */
+function userClick(int x, int y)
 {
-
+	Organism.toggleCell(x,y);
 }
 
-/* function for when the user wants to run the whole simulation out */
-function userRunAll()
+/* function for when the user wants to clear the canvas */
+function userClear()
 {
-
+	Organism.clear();
 }
 
-/* function for when the user wants to stop the simulation from stepping */
-function UserStepEndAll()
+/* function for when the user wants to change the organism they are using */
+function changeSelectedOrg(Organism org)
 {
-
+	this.Organism == org;
 }
 
-/* function for when the user wants to pause the simulation */
-function userPause()
+/* function for when the organism changes from alive or dead */
+function stateChanged()
 {
-
+	//OrganismView.update();
+	//need a way to update OrganismView with new matrix
 }
