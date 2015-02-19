102bce1cbdf3d9f63c4da015e3fb0a9d127e7500
Katpinto <Kpinto@128-223-223-163-wireless.uoregon.edu>
2015-02-18 17:06:53 -0800

    updated

--- controllers/colonyController.js
+++ controllers/colonyController.js
@@ -1,36 +1,38 @@
 /*
 Colony: Colony
 View: ColonyView 
-File owner:
+File owner: Kat
 */
 
+var colony;/*This may be totally unnecessary but it all depends on how we import files*/
 
 /* function for when user chooses an organism*/
-function userSelectOrg()
+function userSelectOrg(int orgID)
 {
-
+	/*orgID = get orgModel*/
+	organismController.changeOrg(orgID);
 }
 
 /* function for when the user chooses to step the simulation */
 function userStepAll()
 {
-
+	colony.stepAll();
 }
 
 /* function for when the user wants to run the whole simulation out */
 function userRunAll()
 {
-
+	colony.runAll();
 }
 
 /* function for when the user wants to stop the simulation from stepping */
 function UserStepEndAll()
 {
-
+	colony.stepEndAll();
 }
 
 /* function for when the user wants to pause the simulation */
 function userPause()
 {
-
+	colony.pause();
 }
