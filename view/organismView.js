9a35e5d6340cf4da4424b9f516129e3cbecbb285
twhite745 <twhite745@gmail.com>
2015-02-18 17:20:27 -0800

    state.js and organismView.js

--- view/organismView.js
+++ view/organismView.js
@@ -1,11 +1,51 @@
 /*
-View: canvas? or canvases?  
+View: canvas
 File owner:
 */
 
+var CANVAS_WIDTH = 600; //Width of Canvas
+var CANVAS_HEIGHT = 600; //Height of Canvas
 
-/* function to update the colony on the canvas */
+var ROWS = 50; //Number of cells in a row
+var COLUMNS = 50; //Number of cells in a column
+
+var COLOR_DEAD = "white"; //Color of dead cells, (0 on the matrix)
+var COLOR_ALIVE = "black"; //Color of dead cells, (1 on the matrix)
+
+var CELL_WIDTH = 10; //Pixel width of cell
+var CELL_HEIGHT = 10; //Pixel height of cell
+
+var state = createMatrix(ROWS, COLUMNS, 0);
+
+/* function to update the organism on the canvas */
 function update()
 {
+	c = canvas.getContext("2d");
+	c.fillStyle = COLOR_ALIVE;
+	c.lineWidth = 1;
+	c.strokeStyle = COLOR_ALIVE;
+	for (var row = 0; row < ROWS; row++) {
+		for (var column = 0; column < COLUMNS; column++) {
+			c.beginPath();
+			var x = column * CELL_WIDTH;
+			var y = row * CELL_HEIGHT;
+			if(state[row][column] == 1){
+				c.fillStyle = COLOR_ALIVE;
+			} else{
+				c.fillStyle = COLOR_DEAD;
+			}
+			c.rect(x, y, CELL_WIDTH, CELL_HEIGHT);
+			c.fill();
+			c.stroke();
+			c.closePath();
+		}
+	}
+}	
+/*
+function handleClick(e) {
+    c.fillStyle = "black";
 
+    c.fillRect(Math.floor(e.offsetX/10)*10,
+               Math.floor(e.offsetY/10)*10,
+               10, 10);
 }
