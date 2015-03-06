41b292b2bcdfbefd99077dff1d40674d0e3e666b
twhite745 <twhite745@gmail.com>
2015-03-05 23:53:51 -0800

    minor progress on Import

--- view/importExportView.js
+++ view/importExportView.js
@@ -1,28 +1,32 @@
 /***********************8
 Deals with importing and exporting
 ************************/
-/*
+
 // Check for the various File API support.
 if (window.File && window.FileReader && window.FileList && window.Blob) {
   // Great success! All the File APIs are supported.
+  document.getElementById('importDisplayError').innerHTML = 'Import supported.';
 } else {
   document.getElementById('importDisplayError').innerHTML = 'Importing and Exporting are not fully supported in this browser.';
 }
 
-  function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
+var fileInput = document.getElementById('fileInput');
+var fileDisplayArea = document.getElementById('fileDisplayArea');
 
-    // files is a FileList of File objects. List some properties.
-    for (var i = 0, f; f = files[i]; i++) {
-			var reader = new FileReader();
-			console.log('hi');
-			reader.onload = function(e){
-				document.getElementById('fileDisplayArea').innerHTML = reader.result;
-			}
-			
-			//reader.readAsText(file);
-	    	//document.getElementById('importError').innerHTML = 'Can only import files with .lif extension';
+fileInput.addEventListener('change', function(e) {
+	var file = fileInput.files[0];
+	var textType = /text.*/;
+
+	if (file.type.match(textType)) {
+		var reader = new FileReader();
+
+		reader.onload = function(e) {
+			fileDisplayArea.innerText = reader.result;
+		}
+
+		reader.readAsText(file);	
+	} else {
+		fileDisplayArea.innerText = "File not supported!"
 	}
+});
 
-  document.getElementById('files').addEventListener('change', handleFileSelect, false);
-*/
