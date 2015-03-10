014cdbbcb05c12024f0d27ea3648eaa8477f4ea9
jadamgreen <jadam.green@gmail.com>
2015-03-10 06:32:20 -0700

    minor

--- simulation/settings.js
+++ simulation/settings.js
@@ -18,85 +18,83 @@ function settings(){
 	/* Setters */
 	this.setBirthArray = function(barray){
 		this.birthArray = barray;
-	}
+	};
 	this.setSustainArray = function(sarray){
 		this.sustainArray = sarray;
-	}
+	};
 	this.setBirthArrayVal = function(neighbs, val){
 		this.birthArray[neighbs] = val;
-	}
+	};
 	this.setSustainArrayVal = function(neighbs, val){
 		this.sustainArray[neighbs] = val;
-	}
+	};
 	this.setSpeed = function(speed){
 		console.log("Setting speed to " + speed);
 		this.speed = speed;
-	}
+	};
 	this.setLifetime = function(lifetime){
 		console.log("Setting lifetime to " + lifetime);
 		this.lifetime = lifetime;
-	}
-	
+	};
 	this.setSpawnWidth = function(w){
 		this.spawnWidth = w;
-	}
+	};
 	this.setSpawnHeight = function(h){
 		this.spawnHeight = h;
-	}	
+	};
 	this.setSpawnDensity = function(d){
 		this.spawnDensity = d;
-	}		
+	};
 	this.setSpawnCenterX = function(cx){
 		this.spawnCenterX = cx;
-	}	
+	};
 	this.setSpawnCenterY = function(cy){
 		this.spawnCenterY = cy;
-	}	
+	};
 
 	/* Toggles */
 	this.toggleBirthArrayVal = function(neighbs){
 		this.birthArray[neighbs] = this.birthArray[neighbs] == 1? 0:1;
 		// alert("User toggled Birth " + neighbs);
-	}
+	};
 	this.toggleSustainArrayVal = function(neighbs){
 		this.sustainArray[neighbs] = this.sustainArray[neighbs] == 1? 0:1;
 		// alert("User toggled Sustain " + neighbs);
-	}
+	};
 
 	/* Getters */
 	this.getBirthArray = function(){
 		return this.birthArray;
-	}
+	};
 	this.getSustainArray = function(){
 		return this.sustainArray;
-	}
+	};
 	this.getBirthArrayVal = function(neighbs){
 		return this.birthArray[neighbs] == 1;
-	}
+	};
 	this.getSustainArrayVal = function(neighbs){
 		return this.sustainArray[neighbs] == 1;
-	}
+	};
 	this.getSpeed = function(){
 		return this.speed;
-	}
+	};
 	this.getLifetime = function(){
 		return this.lifetime;
-	}
-
+	};
 	this.getSpawnWidth = function(){
 		return this.spawnWidth;
-	}
+	};
 	this.getSpawnHeight = function(){
 		return this.spawnHeight;
-	}	
+	};
 	this.getSpawnDensity = function(){
 		return this.spawnDensity;
-	}		
+	};
 	this.getSpawnCenterX = function(){
 		return this.spawnCenterX;
-	}	
+	};
 	this.getSpawnCenterY = function(){
 		return this.spawnCenterY;
-	}	
+	};
 
 }
